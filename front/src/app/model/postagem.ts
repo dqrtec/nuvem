5c17483282225a000a6a7bbe136b7a124afdc19f
victhorplus <victhor-hugo-11@hotmail.com>
2020-04-06 12:47:15 -0300

    crianção do model USER e POSTAGEM e servico USER

---
+++ front/src/app/model/postagem.ts
@@ -0,0 +1,6 @@
+export interface Postagem{
+    apelido: string, // Apelido de quem publicou a postagem
+    src: string,
+    data,
+    curtida: string[] // Array de pelidos de todos que curtiram a foto
+}
